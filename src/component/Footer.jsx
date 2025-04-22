a38e80718b0cae692fde8d8764a86cbedbf1ac94
Aditiv2004 <134513454+Aditiv2004@users.noreply.github.com>
2025-04-22 09:49:08 +0530

    Add files via upload

---
+++ src/component/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    
+    <>
+    <footer className="site-footer">
+  <div className="contact-info">
+    <i className="fas fa-envelope"></i> Contact Me: 
+    <a href="mailto:aditi@example.com">aditi@example.com</a>
+  </div>
+
+  <p className="connect-text">Let's connect on social media!</p>
+
+  <div className="social-icons">
+    <a href="https://instagram.com" target="_blank"><i className="fab fa-instagram"></i></a>
+    <a href="https://facebook.com" target="_blank"><i className="fab fa-facebook"></i></a>
+    <a href="https://github.com" target="_blank"><i className="fab fa-github"></i></a>
+    <a href="https://linkedin.com" target="_blank"><i className="fab fa-linkedin"></i></a>
+  </div>
+
+  <p className="footer-copy">&copy; 2025 <strong>Aditi</strong>. All Rights Reserved.</p>
+</footer>
+
+
+
+    </>
+  )
+}
